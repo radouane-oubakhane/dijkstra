bd043a94bcdfedc7b96961ad08e08a9c0d30a8db
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-02-26 02:43:42 +0100

    add initMat function.

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void initMat(int n, float* matrice) {
+
+    printf("Veuillez saisire la matrice des poids : \n");
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            do {
+                printf("mat(%d, %d) : ", i+1, j+1);
+                scanf("%f", (matrice+i*n+j));
+            } while (*(matrice+i*n+j) < 0);
+        }
+    }
+}
 
 int main( )
 {
@@ -8,7 +24,9 @@ int main( )
     printf("Veuillez saisire le nombre de sommets : ");
     scanf("%d", &n);
 
-
+    matrice = malloc(n*n*sizeof(float));
+    initMat(n, matrice);
+    
 
     return 0;
 }
