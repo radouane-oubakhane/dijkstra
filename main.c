0f3d472ee5a8b1c7ea896749cce8f6bfcafebcb5
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-02-27 21:23:34 +0100

    add printChemin function.

--- main.c
+++ main.c
@@ -31,15 +31,14 @@ int min(int n, float* distance, int* ensembleS) {
     return index;
 }
 
-int* dijkstra(int n, float* matrice) {
+int* dijkstra(int n, float* matrice, int s) {
     int*  ensembleS = malloc(n*sizeof(int));
     int* pred = malloc(n*sizeof(int));
     float* distance = malloc(n*sizeof(float));
 
-    int s, sortie = 1;
+    int sortie = 1;
 
-    printf("Veuillez saisire le sommet de depart : ");
-    scanf("%d", &s);
+
 
     for (int i = 0; i < n; i++) {
         *(ensembleS+i) = 0;
@@ -78,43 +77,68 @@ int* dijkstra(int n, float* matrice) {
     return pred;
 }
 
+void printChemin(int n, int* pred, int dest) {
+
+    int chemin[n];
+    int temp = dest;
+    int index = 1;
+
+    chemin[0] = dest;
+
+    while (1) {
+        temp = *(pred + temp);
+        chemin[index++] = temp;
+        if (temp == 0) break;
+    }
+
+    printf("%d", chemin[index-1]);
+
+    for (int i = index-2; i >= 0; i--) {
+        if (chemin[i] >= 0) {
+            printf(" - %d", chemin[i]);
+            if (chemin[i] == dest) break;
+        }
+    }
+    printf("\n");
+}
 
 int main( )
 {
-    int n;
+    int n, s;
     float* matrice;
     int* pred;
 
     printf("Veuillez saisire le nombre de sommets : ");
     scanf("%d", &n);
 
+    printf("Veuillez saisire le sommet de depart : ");
+    scanf("%d", &s);
+
     matrice = malloc(n*n*sizeof(float));
 
+    /*
     float temp[6][6] = {{0,10,0,0,0,9},
                         {10,0,5,8,13,0},
                         {0,5,0,4,0,3},
 
                         {0,8,4,0,2,5},
                         {0,13,0,2,0,15},
-                        {9,0,3,5,15,0}
-                    };
+                        {9,0,3,5,15,0}};
 
-    // initMat(n, matrice);
+    */
 
+    initMat(n, matrice);
 
+    pred = dijkstra(n, matrice, s);
 
+    printf("------------------------------ - Les Chemins - ------------------------------\n");
 
-
-    pred = dijkstra(n, temp);
-
-    printf("-----------------");
     for (int i = 0; i < n; ++i) {
-        printf("%d\n", *(pred+i));
+        if (i != s) {
+            printf("Le plus court chemin à partir du sommet %d au sommet %d est : ", s, i);
+            printChemin(n, pred, i);
+        }
     }
-
-
-
-
 
     free(matrice);
     free(pred);
